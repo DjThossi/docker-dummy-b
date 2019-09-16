ae14d275980e4d0f7fe84224f5e3627e71f28ada
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2019-09-16 13:06:59 +0200

    Innitial commit

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Docker Dummy B</title>
+</head>
+<body bgcolor="#F9F9F9">
+    <div id="better">
+        <a href="https://better.group" target="_blank">Better Logo</a>
+    </div>
+    <div id="smest-it">
+        <a href="https://www.smest.it" target="_blank">Smest.it Logo</a>
+    </div>
+    <div id="dummy">
+        <a href="<?= getenv('LINK_TO_DOCKER_DUMMY_A') ?>" target="_blank">Docker Dummy A</a>
+    </div>
+</body>
+</html>
